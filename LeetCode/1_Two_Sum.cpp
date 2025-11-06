bcad2ba25a74c85ab8d1f7a6e399762e83220f9a
Efat Ahmed <efatahmed2005@gmail.com>
2025-11-07 02:51:22 +0600

    LeetCode 01-> Two Sum

---
+++ LeetCode/1_Two_Sum.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> m;
+        int complement;
+        for (auto i = 0; i < nums.size(); i++)
+        {
+            complement = target - nums[i];
+            if (m.find(complement) != m.end())
+                return {m[complement], i};
+            m.emplace(nums[i], i);
+        }
+        return {};
+    }
+};
