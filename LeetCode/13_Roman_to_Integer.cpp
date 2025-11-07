fa249ce006b73cce5d885bd1ecf7689cd15928f7
Efat Ahmed <efatahmed2005@gmail.com>
2025-11-07 14:57:35 +0600

    LeetCode 13->13. Roman to Integer

---
+++ LeetCode/13_Roman_to_Integer.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        unordered_map<string, int> m;
+        m.emplace("I", 1);
+        m.emplace("V", 5);
+        m.emplace("X", 10);
+        m.emplace("L", 50);
+        m.emplace("C", 100);
+        m.emplace("D", 500);
+        m.emplace("M", 1000);
+        m.emplace("IV", 4);
+        m.emplace("IX", 9);
+        m.emplace("XL", 40);
+        m.emplace("XC", 90);
+        m.emplace("CD", 400);
+        m.emplace("CM", 900);
+        int i = 0, sum = 0;
+        while (i != s.length())
+        {
+            bool found = false;
+            if (i + 1 < s.length())
+            {
+                if (m.count(s.substr(i, 2)))
+                {
+                    found = true;
+                    sum += m[s.substr(i, 2)];
+                    i += 2;
+                }
+            }
+            if (!found)
+            {
+                sum += m[s.substr(i, 1)];
+                i++;
+            }
+        }
+        return sum;
+    }
+};
