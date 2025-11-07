54c0b060af064270d85fcf738375b2291ab0c695
Efat Ahmed <efatahmed2005@gmail.com>
2025-11-07 14:48:55 +0600

    LeetCode 11->11. Container With Most Water

---
+++ LeetCode/11_Container_With_Most_Water.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int lp = 0, rp = height.size() - 1, w, ans = 0, h;
+        while (rp > lp)
+        {
+            w = rp - lp;
+            h = min(height[lp], height[rp]);
+            ans = max(ans, (w * h));
+
+            if (height[lp] < height[rp])
+                lp++;
+            else
+                rp--;
+        }
+        return ans;
+    }
+};
